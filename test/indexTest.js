3963ceaa380da23bfbf62fe05ebbeb1b9f311418
dzc0d3r <m4dcod3r@protonmail.com>
2023-09-08 18:28:22 +0100

    Finished assignement :tada:

--- test/indexTest.js
+++ test/indexTest.js
@@ -29,7 +29,6 @@ describe( "Deciduous", () => {
 
   it( 'has a static method `definition` that uses `super.definition` and expands on it', () => {
     expect( Deciduous.definition() )
-      .to.eql( "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Deciduous trees shed their leaves annually." )
   } )
 
 } )
